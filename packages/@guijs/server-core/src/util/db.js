d68dbb489146f15f7173272cb3e2a8a3140112cf
Guillaume Chau <alphadelta.fg@gmail.com>
2019-07-12 19:53:17 +0200

    chore: db log

--- packages/@guijs/server-core/src/util/db.js
+++ packages/@guijs/server-core/src/util/db.js
@@ -2,8 +2,11 @@ const Lowdb = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 const path = require('path')
 const { rcFolder } = require('./rcFolder')
+const { log } = require('./logger')
 
 const db = new Lowdb(new FileSync(path.resolve(rcFolder, 'db.json')))
+
+log('Loading DB...', path.resolve(rcFolder, 'db.json'))
 
 // Seed an empty DB
 db.defaults({
