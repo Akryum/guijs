28fa28a3af60123b3143372cf535d432678d6b06
Guillaume Chau <alphadelta.fg@gmail.com>
2019-07-12 19:52:30 +0200

    refactor: put dev live folder at repo root

--- packages/@guijs/server-core/src/util/rcFolder.js
+++ packages/@guijs/server-core/src/util/rcFolder.js
@@ -7,14 +7,13 @@ const { log } = require('./logger')
 let folder
 
 if (process.env.GUIJS_TEST) {
-  folder = path.resolve(__dirname, '../../../live-test')
+  folder = path.resolve(process.cwd(), '../../../live-test')
   // Clean DB
-  fs.removeSync(path.resolve(__dirname, folder))
+  fs.removeSync(folder)
 } else if (process.env.GUIJS_DEV) {
-  folder = path.resolve(__dirname, '../../../live')
+  folder = path.resolve(process.cwd(), '../../../live')
 } else {
-  folder = (process.env.GUIJS_DB_PATH &&
-    path.resolve(__dirname, process.env.GUIJS_DB_PATH)) ||
+  folder = process.env.GUIJS_DB_PATH ||
     getRcPath('.guijs', { xdgFolder: 'guijs' })
   migrateRcFolder()
 }
@@ -23,12 +22,11 @@ ensureFolder()
 exports.rcFolder = folder
 
 function ensureFolder () {
-  fs.ensureDirSync(path.resolve(__dirname, folder))
+  fs.ensureDirSync(folder)
 }
 
 function migrateRcFolder () {
-  const oldFolder = (process.env.VUE_CLI_UI_DB_PATH &&
-    path.resolve(__dirname, process.env.VUE_CLI_UI_DB_PATH)) ||
+  const oldFolder = process.env.VUE_CLI_UI_DB_PATH ||
     getRcPath('.vue-cli-ui', { xdgFolder: 'vue' })
 
   if (fs.existsSync(oldFolder) && !fs.existsSync(folder)) {
