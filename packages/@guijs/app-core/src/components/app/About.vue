30fa2347623a3c6c819668b31a97c475dc23e305
Guillaume Chau <alphadelta.fg@gmail.com>
2019-07-21 19:18:43 +0200

    refactor: about page

--- packages/@guijs/app-core/src/components/app/About.vue
+++ packages/@guijs/app-core/src/components/app/About.vue
@@ -1,22 +1,16 @@
 <template>
   <div class="about page">
-    <h1>{{ $t('org.vue.views.about.title') }}</h1>
+    <div class="logo">
+      <img class="logo-image" src="@/assets/guijs.svg" alt="guijs logo">
+    </div>
+
+    <h1>guijs</h1>
 
     <p class="description" v-html="$t('org.vue.views.about.description')"/>
 
-    <blockquote class="quote" v-text="$t('org.vue.views.about.quote')"/>
-
-    <p>- <a href="https://github.com/Akryum" target="_blank">Guillaume Chau</a></p>
-
-    <h2>{{ $t('org.vue.views.about.links') }}</h2>
-
     <div class="links vue-ui-grid default-gap">
-      <VueButton href="https://github.com/vuejs/vue-cli" target="_blank">Repository</VueButton>
-      <VueButton href="https://cli.vuejs.org" target="_blank">Documentation</VueButton>
-      <VueButton href="https://vuejs.org/v2/guide/" target="_blank">Vue.js Guide</VueButton>
-      <VueButton href="https://vuejs.org/v2/api/" target="_blank">Vue.js API reference</VueButton>
-      <VueButton href="https://forum.vuejs.org/" target="_blank">Vue.js Forums</VueButton>
-      <VueButton href="https://chat.vuejs.org/" target="_blank">Vue.js Chat</VueButton>
+      <VueButton href="https://guijs.dev" target="_blank" class="big">Home</VueButton>
+      <VueButton href="https://github.com/Akryum/guijs" target="_blank" class="big">Repository</VueButton>
     </div>
 
     <div class="actions">
@@ -55,18 +49,28 @@ export default {
 <style lang="stylus" scoped>
 .about
   padding 42px
-  max-width 840px
   margin 0 auto
   box-sizing border-box
+  height 100%
+  display flex
+  flex-direction column
+  align-items center
+  justify-content center
 
-  .quote
-    border-left solid 4px rgba($vue-ui-color-primary, .3)
-    margin-left 0
-    padding-left $padding-item
-    color lighten($vue-ui-color-dark, 30%)
+  .logo
+    width 128px
+    height @width
+
+    .logo-image
+      width 100%
+      height @width
+
+  .description
+    margin-top $padding-item
 
   .links
-    grid-template-columns repeat(auto-fill, 180px)
+    margin-top @padding
+    grid-template-columns repeat(2, 180px)
 
   .actions
     margin-top @padding
