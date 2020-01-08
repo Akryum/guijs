384309b05db4ceae8ed8c3b354450fcc8e67d6b5
Guillaume Chau <alphadelta.fg@gmail.com>
2020-01-08 22:32:48 +0100

    chore: update generated context type

--- packages/@guijs/server-core/src/generated/context.d.ts
+++ packages/@guijs/server-core/src/generated/context.d.ts
@@ -8,6 +8,8 @@ import ProjectConfigBase from './config'
 // Plugins
 import NodepackPluginApollo from '@nodepack/plugin-apollo/types/context'
 import NodepackPluginExpress from '@nodepack/plugin-express/types/context'
+// Project context files
+import Db from '@/context/db'
 
 interface ContextBase {
   config: ProjectConfigBase
@@ -16,5 +18,6 @@ interface ContextBase {
 export type Context = ContextBase
   & NodepackPluginApollo
   & NodepackPluginExpress
+  & Db
 
 export default Context
