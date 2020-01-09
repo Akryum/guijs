c16fa15b6c0cb6f4b9c3ff970f80f6e9c48e45a0
Guillaume Chau <alphadelta.fg@gmail.com>
2020-01-09 19:29:09 +0100

    feat: VEmpty

---
+++ packages/@guijs/frontend-ui/components/Empty.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: null,
+    },
+  },
+}
+</script>
+
+<template>
+  <div>
+    <div class="flex flex-col items-center justify-center p-4 h-full">
+      <i
+        v-if="icon"
+        class="material-icons text-gray-400"
+      >{{ icon }}</i>
+      <div class="flex flex-col items-center text-gray-500">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
