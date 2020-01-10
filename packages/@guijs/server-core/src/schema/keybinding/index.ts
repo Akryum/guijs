455a2d9c52d6b8fab64dca07c94414f338dc031d
Guillaume Chau <alphadelta.fg@gmail.com>
2020-01-10 23:12:10 +0100

    fix: change default toggle terminals keybinding

--- packages/@guijs/server-core/src/schema/keybinding/index.ts
+++ packages/@guijs/server-core/src/schema/keybinding/index.ts
@@ -32,7 +32,7 @@ export const keybindings = [
   {
     id: 'toggle-terminals',
     description: 'guijs.status-bar.toggle-terminals',
-    sequences: ['mod+shift+¥', 'mod+alt+k t'],
+    sequences: ['mod+shift+;', 'mod+alt+k t'],
     scope: 'root',
     global: true,
   },
