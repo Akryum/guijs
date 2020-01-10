8306764ed45379d0edb2cd03eedc2e46765e2ce2
Guillaume Chau <alphadelta.fg@gmail.com>
2020-01-10 12:05:17 +0100

    feat: sidepane

--- packages/@guijs/frontend-core/src/components/App.vue
+++ packages/@guijs/frontend-core/src/components/App.vue
@@ -1,8 +1,10 @@
 <script>
+import SidePane from './side-pane/SidePane.vue'
 import StatusBar from './status-bar/StatusBar.vue'
 
 export default {
   components: {
+    SidePane,
     StatusBar,
   },
 }
@@ -13,7 +15,9 @@ export default {
     id="app"
     class="w-screen h-screen overflow-hidden flex flex-col items-stretch"
   >
-    <div class="flex-1 flex flex-col overflow-hidden">
+    <div class="flex-1 flex overflow-hidden">
+      <SidePane class="flex-none" />
+
       <router-view class="flex-1 overflow-hidden" />
     </div>
 
