ea83beecef262ae6f8da24a7c06713391b9c5cec
Guillaume Chau <alphadelta.fg@gmail.com>
2020-01-16 23:46:28 +0100

    feat: useSetting

--- packages/@guijs/frontend-core/src/util/setting.js
+++ packages/@guijs/frontend-core/src/util/setting.js
@@ -1,5 +1,6 @@
 import { apolloClient } from '@/apollo'
 import gql from 'graphql-tag'
+import { useSubscription, useQuery, useResult } from '@vue/apollo-composable'
 
 export async function updateSetting (id, value) {
   await apolloClient.mutate({
@@ -18,4 +19,38 @@ export async function updateSetting (id, value) {
       },
     },
   })
+}
+
+export function useSetting (id) {
+  const { result } = useQuery(gql`
+    query darkModeSetting ($id: ID!) {
+      setting (id: $id) {
+        id
+        value
+      }
+    }
+  `, {
+    id,
+  })
+  const setting = useResult(result, null, data => data.setting.value)
+
+  useSubscription(gql`
+    subscription darkModeSettingUpdated ($id: ID!) {
+      settingUpdated (id: $id) {
+        id
+        value
+      }
+    }
+  `, {
+    id,
+  })
+
+  async function update (value) {
+    return updateSetting(id, value)
+  }
+
+  return {
+    setting,
+    update,
+  }
 }
