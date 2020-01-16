77f7be619d1331f83d076891a976b860965e8b6d
Guillaume Chau <alphadelta.fg@gmail.com>
2020-01-16 23:24:19 +0100

    fix: apply dark mode even earlier

--- packages/@guijs/frontend-core/src/components/service/DarkModeService.vue
+++ packages/@guijs/frontend-core/src/components/service/DarkModeService.vue
@@ -7,29 +7,29 @@ import { watch } from '@vue/composition-api'
 
 const STORAGE_DARK_MODE = 'dev.guijs.darkMode'
 
+function applyDarkMode (value) {
+  const el = document.documentElement
+  if (value) {
+    el.classList.add('mode-dark')
+  } else {
+    el.classList.remove('mode-dark')
+  }
+  localStorage.setItem(STORAGE_DARK_MODE, JSON.stringify(value))
+}
+
+// LocalStorage to apply instantly
+
+try {
+  const storageValue = localStorage.getItem(STORAGE_DARK_MODE)
+  if (storageValue) {
+    applyDarkMode(JSON.parse(storageValue))
+  }
+} catch (e) {
+  console.error(e)
+}
+
 export default {
   setup () {
-    function applyDarkMode (value) {
-      const el = document.documentElement
-      if (value) {
-        el.classList.add('mode-dark')
-      } else {
-        el.classList.remove('mode-dark')
-      }
-      localStorage.setItem(STORAGE_DARK_MODE, JSON.stringify(value))
-    }
-
-    // LocalStorage to apply instantly
-
-    try {
-      const storageValue = localStorage.getItem(STORAGE_DARK_MODE)
-      if (storageValue) {
-        applyDarkMode(JSON.parse(storageValue))
-      }
-    } catch (e) {
-      console.error(e)
-    }
-
     // GraphQL
 
     const { result } = useQuery(gql`
