94752cc8667a0d1c143914e9d084da9178fc7a07
Guillaume Chau <alphadelta.fg@gmail.com>
2020-01-17 09:45:55 +0100

    feat(ui): VError

---
+++ packages/@guijs/frontend-ui/components/Error.vue
@@ -0,0 +1,43 @@
+<script>
+import { computed } from '@vue/composition-api'
+export default {
+  props: {
+    error: {
+      type: [String, Error, Object],
+      default: null,
+    },
+  },
+
+  setup (props) {
+    return {
+      message: computed(() => {
+        // GraphQL
+        {
+          const messages = []
+          if (props.error.graphQLErrors) {
+            messages.push(...props.error.graphQLErrors.map(e => e.message))
+          }
+          if (props.error.networkError) {
+            messages.push(props.error.networkError)
+          }
+          if (messages.length) return messages.join('\n')
+        }
+
+        return props.error.message || props.error
+      }),
+    }
+  },
+}
+</script>
+
+<template>
+  <div
+    v-if="error"
+    class="flex items-center text-red-500 p-4 my-6 border-red-300 dark:border-red-700 border rounded"
+  >
+    <i class="material-icons mr-3">error</i>
+    <div class="whitespace-pre">
+      {{ $t(message) }}
+    </div>
+  </div>
+</template>
