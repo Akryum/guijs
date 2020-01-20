b0458189ef7f72b68de3118d82bd199c4a59d212
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-01-20 18:08:16 +0100

    feat(context): getProject()

--- packages/@guijs/server-core/src/context/projectId.ts
+++ packages/@guijs/server-core/src/context/projectId.ts
@@ -1,4 +1,5 @@
 import { onCreate } from '@nodepack/app-context'
+import { MetaProject } from '@/schema/project/meta-types'
 
 onCreate(context => {
   context.getProjectId = function () {
@@ -9,8 +10,15 @@ onCreate(context => {
       return this.req.headers['project-id']
     }
   }
+
+  context.getProject = function () {
+    const projectId = this.getProjectId()
+    if (!projectId) return null
+    return this.db.projects.findOne({ _id: projectId })
+  }
 })
 
 export default interface ProjectIdContext {
   getProjectId: () => string
+  getProject: () => MetaProject
 }
