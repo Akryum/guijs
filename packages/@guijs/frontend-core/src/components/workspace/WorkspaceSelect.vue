e925f45ec187c4d0159b8f75d1651c8dce4d7476
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-01-21 19:35:25 +0100

    feat(workspaces): current workspace

--- packages/@guijs/frontend-core/src/components/workspace/WorkspaceSelect.vue
+++ packages/@guijs/frontend-core/src/components/workspace/WorkspaceSelect.vue
@@ -2,6 +2,9 @@
 import WorkspaceList from './WorkspaceList.vue'
 import { ref, watch } from '@vue/composition-api'
 import { useRouter, useRoute } from '@/util/router'
+import { useQuery, useResult } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+import { projectWorkspaceFragment } from './fragments'
 
 export default {
   components: {
@@ -33,9 +36,29 @@ export default {
       }
     })
 
+    // Current workspace
+    const { result } = useQuery(gql`
+      query currentWorkspace ($projectId: ID!, $workspaceId: ID!) {
+        project (id: $projectId) {
+          id
+          workspace (id: $workspaceId) {
+            ...projectWorkspace
+          }
+        }
+      }
+      ${projectWorkspaceFragment}
+    `, () => ({
+      projectId: route.value.params.projectId,
+      workspaceId: route.value.params.workspaceId,
+    }), () => ({
+      enabled: !!route.value.params.workspaceId,
+    }))
+    const currentWorkspace = useResult(result, null, data => data.project.workspace)
+
     return {
       isOpen,
       select,
+      currentWorkspace,
     }
   },
 }
@@ -45,15 +68,29 @@ export default {
   <VPopper
     :open.sync="isOpen"
   >
-    <div class="flex">
+    <div class="overflow-hidden">
       <VButton
         iconRight="keyboard_arrow_down"
-        class="flex-1 btn-md h-72p hover:bg-primary-100 dark-hover:bg-primary-900"
+        class="w-full btn-md h-72p hover:bg-primary-100 dark-hover:bg-primary-900"
         square
         align="left"
         extend
       >
-        Workspace
+        <template v-if="currentWorkspace">
+          <img
+            :src="currentWorkspace.type.logo"
+            :alt="currentWorkspace.type.name"
+            class="w-6 h-6 rounded mr-4 flex-none"
+          >
+          <div
+            class="flex-1 w-0 truncate text-left leading-normal"
+          >
+            {{ currentWorkspace.name }}
+          </div>
+        </template>
+        <template v-else>
+          ...
+        </template>
       </VButton>
     </div>
 
