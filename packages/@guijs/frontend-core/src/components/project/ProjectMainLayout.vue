291a0a0296aaca4509deda0d555930e14a0fb580
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-01-22 01:08:32 +0100

    fix(layout): project main layout fixes

--- packages/@guijs/frontend-core/src/components/project/ProjectMainLayout.vue
+++ packages/@guijs/frontend-core/src/components/project/ProjectMainLayout.vue
@@ -9,7 +9,7 @@ export default {
 </script>
 
 <template>
-  <div class="flex flex-col h-full">
+  <div class="flex flex-col h-full overflow-hidden">
     <!-- Header -->
     <header
       v-if="!$route.matched.some(m => m.meta.hideHeader)"
@@ -17,26 +17,26 @@ export default {
     >
       <div
         v-if="!$route.matched.some(m => m.meta.hideWorkspaceSelect)"
-        class="w-64 border-gray-200 dark:border-gray-950 border-r"
+        class="flex-none w-64 border-gray-200 dark:border-gray-950 border-r"
       >
         <WorkspaceSelect
           class="h-full"
         />
       </div>
-      <div>
+      <div class="flex-1">
         <router-view name="header" />
       </div>
     </header>
 
     <!-- Content -->
-    <div class="flex-1 flex items-stretch">
+    <div class="flex-1 flex items-stretch overflow-hidden">
       <aside
         v-if="!$route.matched.some(m => m.meta.hideAside)"
-        class="w-64 border-gray-200 dark:border-gray-950 border-r"
+        class="flex-none w-64 border-gray-200 dark:border-gray-950 border-r overflow-y-auto"
       >
         <router-view name="aside" />
       </aside>
-      <main>
+      <main class="flex-1 overflow-y-auto">
         <router-view />
       </main>
     </div>
