4b4d805994e993a45612dbd5ac1be1d1fb829912
Lucas Nogueira <lucas@quasar.dev>
2020-02-01 09:27:22 -0300

    feat(tauri) spawn node server and point to the correct UI dist folder

--- packages/@guijs/tauri-app/src-tauri/src/main.rs
+++ packages/@guijs/tauri-app/src-tauri/src/main.rs
@@ -9,23 +9,46 @@ mod cmd;
 extern crate serde_derive;
 extern crate serde_json;
 
+use std::io::{BufRead, BufReader};
+
 fn main() {
-  tauri::AppBuilder::new()
-    .invoke_handler(|_webview, arg| {
-      use cmd::Cmd::*;
-      match serde_json::from_str(arg) {
-        Err(_) => {}
-        Ok(command) => {
-          match command {
-            // definitions for your custom commands from Cmd here
-            MyCustomCommand { argument } => {
-              //  your command code
-              println!("{}", argument);
+  let resource_dir = tauri::api::platform::resource_dir().expect("failed to get resource dir");
+  let server_path = resource_dir.join("server/app.js");
+  let stdout = std::process::Command::new("node")
+    .env("PORT", "4000")
+    .args(vec!(server_path))
+    .stdout(std::process::Stdio::piped())
+    .spawn()
+    .expect("Failed to guijs server")
+    .stdout.expect("Failed to get guijs server stdout");
+  let reader = BufReader::new(stdout);
+  let mut webview_started = false;
+
+  reader
+    .lines()
+    .filter_map(|line| line.ok())
+    .for_each(|line| {
+      println!("SERVER: {}", line);
+      if !webview_started {
+        webview_started = true;
+        tauri::AppBuilder::new()
+          .invoke_handler(|_webview, arg| {
+            use cmd::Cmd::*;
+            match serde_json::from_str(arg) {
+              Err(_) => {}
+              Ok(command) => {
+                match command {
+                  // definitions for your custom commands from Cmd here
+                  MyCustomCommand { argument } => {
+                    //  your command code
+                    println!("{}", argument);
+                  }
+                }
+              }
             }
-          }
-        }
+          })
+          .build()
+          .run();
       }
-    })
-    .build()
-    .run();
+    });
 }
