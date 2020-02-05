7487b4ea5d0c22cffc97b02ae4635a6097442f18
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-02-05 17:31:43 +0100

    feat(script): basic status indicator

--- packages/@guijs/frontend-core/src/components/script/ScriptStatusIndicator.vue
+++ packages/@guijs/frontend-core/src/components/script/ScriptStatusIndicator.vue
@@ -1,4 +1,6 @@
 <script>
+import { useQuery, useResult } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
 export default {
   props: {
     scriptId: {
@@ -6,11 +8,40 @@ export default {
       required: true,
     },
   },
+
+  setup (props) {
+    const { result } = useQuery(gql`
+      query scriptStatus ($scriptId: ID!) {
+        script (id: $scriptId) {
+          id
+          status
+        }
+      }
+    `, props)
+    const status = useResult(result, 'idle', data => data.script.status)
+
+    return {
+      status,
+    }
+  },
 }
 </script>
 
 <template>
   <div class="w-6 h-6 flex items-center justify-center">
-    <div class="w-3 h-3 bg-gray-500 rounded-full" />
+    <div
+      v-if="status === 'idle'"
+      class="w-3 h-3 bg-gray-500 rounded-full"
+    />
+
+    <div
+      v-if="status === 'running'"
+      class="w-6 h-6 border-blue-400 dark:border-blue-600 border-4 rounded-full"
+    />
+
+    <img
+      v-if="status !== 'idle' && status !== 'running'"
+      :src="require(`@/assets/script-${status}.svg`)"
+    >
   </div>
 </template>
