62e0a96d4062a914d4667a37353c92871d464e52
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-02-05 17:59:51 +0100

    fix(script): overflow

--- packages/@guijs/frontend-core/src/components/script/ScriptView.vue
+++ packages/@guijs/frontend-core/src/components/script/ScriptView.vue
@@ -18,10 +18,10 @@ export default {
 </script>
 
 <template>
-  <div class="flex flex-col h-full">
+  <div class="flex flex-col h-full overflow-hidden">
     <ScriptToolbar class="flex-none" />
 
-    <div class="flex-1 mx-6 mb-6">
+    <div class="flex-1 h-0 mx-6 mb-6">
       <ScriptOutput class="h-full" />
     </div>
   </div>
