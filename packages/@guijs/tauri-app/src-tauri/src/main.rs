a0c349d8d353dbfa493b629c1fe964a53eb3fe48
Lucas Nogueira <lucas@quasar.dev>
2020-02-08 11:41:22 -0300

    feat(tauri) use the splashscreen API

--- packages/@guijs/tauri-app/src-tauri/src/main.rs
+++ packages/@guijs/tauri-app/src-tauri/src/main.rs
@@ -12,43 +12,37 @@ extern crate serde_json;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-  let resource_dir = tauri::api::platform::resource_dir().expect("failed to get resource dir");
-  let server_path = resource_dir.join("server/app.js");
-  let stdout = std::process::Command::new("node")
-    .env("PORT", "4000")
-    .args(vec!(server_path))
-    .stdout(std::process::Stdio::piped())
-    .spawn()
-    .expect("Failed to guijs server")
-    .stdout.expect("Failed to get guijs server stdout");
-  let reader = BufReader::new(stdout);
-  let mut webview_started = false;
+  tauri::AppBuilder::new()
+    .splashscreen_html(include_str!("../splash.html"))
+    .setup(|webview, source| {
+      if source == "splashscreen" {
+        let handle = webview.handle();
+        std::thread::spawn(move || {
+          let mut webview_started = false;
+          let resource_dir = tauri::api::platform::resource_dir().expect("failed to get resource dir");
+          let server_path = resource_dir.join("server/app.js");
+          let stdout = std::process::Command::new("node")
+            .env("PORT", "4000")
+            .args(vec!(server_path))
+            .stdout(std::process::Stdio::piped())
+            .spawn()
+            .expect("Failed to guijs server")
+            .stdout.expect("Failed to get guijs server stdout");
+          let reader = BufReader::new(stdout);
 
-  reader
-    .lines()
-    .filter_map(|line| line.ok())
-    .for_each(|line| {
-      println!("SERVER: {}", line);
-      if !webview_started {
-        webview_started = true;
-        tauri::AppBuilder::new()
-          .invoke_handler(|_webview, arg| {
-            use cmd::Cmd::*;
-            match serde_json::from_str(arg) {
-              Err(_) => {}
-              Ok(command) => {
-                match command {
-                  // definitions for your custom commands from Cmd here
-                  MyCustomCommand { argument } => {
-                    //  your command code
-                    println!("{}", argument);
-                  }
-                }
+          reader
+            .lines()
+            .filter_map(|line| line.ok())
+            .for_each(|line| {
+              println!("SERVER: {}", line);
+              if !webview_started {
+                webview_started = true;
+                tauri::close_splashscreen(&handle).expect("failed to close splashscreen");
               }
-            }
-          })
-          .build()
-          .run();
+            });
+        });
       }
-    });
+    })
+    .build()
+    .run();
 }
