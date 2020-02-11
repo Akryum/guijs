e133e5c45fc9279bff1109c37de107a580b10e0f
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-02-11 22:45:06 +0100

    feat(script): more animation

--- packages/@guijs/frontend-core/src/components/script/ScriptStatusIndicator.vue
+++ packages/@guijs/frontend-core/src/components/script/ScriptStatusIndicator.vue
@@ -1,4 +1,5 @@
 <script>
+/* eslint-disable */
 import { useQuery, useResult } from '@vue/apollo-composable'
 import gql from 'graphql-tag'
 export default {
@@ -28,31 +29,34 @@ export default {
 </script>
 
 <template>
-  <div class="w-6 h-6 flex items-center justify-center relative">
+  <div
+    class="w-6 h-6 flex items-center justify-center relative"
+    :class="`status-${status}`"
+  >
     <div
-      v-if="status === 'idle'"
-      key="idle"
-      class="w-3 h-3 bg-gray-500 rounded-full"
-    />
-
-    <div
-      v-if="status === 'running'"
-      key="running"
-      class="w-5 h-5 border-blue-400 dark:border-blue-600 border-4 rounded-full"
-    />
+      key="dot"
+      class="dot-wrapper absolute z-10"
+    >
+      <div
+        class="dot rounded-full"
+        :class="{
+          'w-3 h-3 border-gray-500': status === 'idle' || status === 'killed',
+          'w-5 h-5 border-teal-400 dark:border-teal-600': status === 'running',
+        }"
+      />
+    </div>
 
     <img
-      v-if="status !== 'idle' && status !== 'running'"
+      v-if="status === 'success' || status === 'error'"
       :src="require(`@/assets/script-${status}.svg`)"
       class="icon"
     >
 
     <transition duration="150">
       <div
-        v-if="status !== 'idle' && status !== 'running' && status !== 'killed'"
-        key="icon-bg"
+        v-if="status === 'success' || status === 'error'"
+        key="icon-bg z-10"
         class="icon-bg absolute"
-        :class="`status-${status}`"
       >
         <div class="animation w-5 h-5 rounded-full" />
       </div>
@@ -61,6 +65,19 @@ export default {
 </template>
 
 <style lang="postcss" scoped>
+.dot {
+  transition: width .15s, height .15s, border-width .15s;
+
+  .status-idle &,
+  .status-killed & {
+    border-width: 6px;
+  }
+
+  .status-running & {
+    border-width: 4px;
+  }
+}
+
 .icon {
   animation: icon-in 0s .15s backwards;
 }
@@ -84,7 +101,6 @@ export default {
 }
 
 .icon-bg {
-  @apply z-10;
   animation: icon-out .15s .15s forwards;
 
   .animation {
@@ -92,11 +108,11 @@ export default {
     border-width: 10px;
   }
 
-  &.status-success .animation {
+  .status-success & .animation {
     @apply border-green-500;
   }
 
-  &.status-error .animation {
+  .status-error & .animation {
     @apply border-red-500;
   }
 
