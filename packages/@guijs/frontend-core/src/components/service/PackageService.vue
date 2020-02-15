384e6e0f239db850dfd0087bfa00fb39e51a750f
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-02-15 15:09:07 +0100

    fix(package): install command workspaceId in payload

--- packages/@guijs/frontend-core/src/components/service/PackageService.vue
+++ packages/@guijs/frontend-core/src/components/service/PackageService.vue
@@ -1,6 +1,6 @@
 <script>
 import { onCommand, runCommand } from '@/util/command'
-import { useRouter } from '@/util/router'
+import { useRouter, useRoute } from '@/util/router'
 import { useSubscription } from '@vue/apollo-composable'
 import gql from 'graphql-tag'
 import { projectPackageFragment } from '../pkg/fragments'
@@ -8,6 +8,7 @@ import { projectPackageFragment } from '../pkg/fragments'
 export default {
   setup () {
     const router = useRouter()
+    const route = useRoute()
 
     onCommand('show-package', async (cmd, payload) => {
       router.push({
@@ -39,6 +40,7 @@ export default {
         if (event.data.awesomeInstall) {
           runCommand('install-package', {
             packageName: event.data.awesomeInstall,
+            workspaceId: route.value.params.workspaceId,
           })
         }
       }
