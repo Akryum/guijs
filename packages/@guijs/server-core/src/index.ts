7fec9bc13003523fdb7373eabf43de942efb45a8
Lucas Nogueira <lucas@quasar.dev>
2020-02-16 17:51:59 -0300

    feat(server-core) add --version arg to the CLI, add bin to package.json

--- packages/@guijs/server-core/src/index.ts
+++ packages/@guijs/server-core/src/index.ts
@@ -1,18 +1,23 @@
 import { bootstrap, printReady } from '@nodepack/app'
 import { hook } from '@nodepack/app-context'
 import { spawn } from 'child_process'
+import { version } from '../package.json'
 
-bootstrap(() => {
-  // @TODO remove printReady
-  printReady()
-})
-
-if (process.env.NODE_ENV !== 'production') {
-  // Auto-generate shcema code
-  hook('apolloListen', () => {
-    spawn('yarn', ['schema-gen'], {
-      cwd: process.cwd(),
-      stdio: ['inherit', 'inherit', 'inherit'],
-    })
+if (process.argv.length === 3 && process.argv[2] === '--version') {
+  console.log(version)
+} else {
+  bootstrap(() => {
+    // @TODO remove printReady
+    printReady()
   })
+
+  if (process.env.NODE_ENV !== 'production') {
+    // Auto-generate shcema code
+    hook('apolloListen', () => {
+      spawn('yarn', ['schema-gen'], {
+        cwd: process.cwd(),
+        stdio: ['inherit', 'inherit', 'inherit'],
+      })
+    })
+  }
 }
