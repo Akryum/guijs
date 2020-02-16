deb05f9b4fd3d7d664b416480324e6f5d2a4dcd8
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-02-16 18:24:26 +0100

    refactor(apollo cache): use getCacheKey

--- packages/@guijs/frontend-core/src/cache.js
+++ packages/@guijs/frontend-core/src/cache.js
@@ -1,11 +1,10 @@
-import { toIdValue } from 'apollo-utilities'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 export const cache = new InMemoryCache({
   cacheRedirects: {
     Query: {
-      projectType: (_, { id }) => toIdValue(cache.config.dataIdFromObject({ __typename: 'ProjectType', id })),
-      script: (_, { id }) => toIdValue(cache.config.dataIdFromObject({ __typename: 'NpmScript', id })),
+      projectType: (_, { id }, { getCacheKey }) => getCacheKey({ __typename: 'ProjectType', id }),
+      script: (_, { id }, { getCacheKey }) => getCacheKey({ __typename: 'NpmScript', id }),
     },
   },
 })
