462ae7d4e0e1967232b2dc3390f53b2b0e5bbb94
Lucas Nogueira <lucas@quasar.dev>
2020-02-20 11:56:26 -0300

    feat(tauri) use which to resolve command paths

--- packages/@guijs/tauri-app/src-tauri/src/main.rs
+++ packages/@guijs/tauri-app/src-tauri/src/main.rs
@@ -16,8 +16,6 @@ use tauri::Handle;
 use serde::Deserialize;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
-use regex::Regex;
-use lazy_static::lazy_static;
 
 #[derive(PartialEq, Deserialize, Clone, Debug)]
 #[serde(tag = "engines", rename_all = "camelCase")]
@@ -46,66 +44,70 @@ fn main() {
         setup = true;
         let handle = webview.handle();
 
-        // check if node exists & matches the minimum version
-        let node_version_output = Command::new(orchestrator_command())
-          .args(vec!("version", "node"))
-          .stdout(Stdio::piped())
-          .output();
-        if node_version_output.is_ok() {
-          let node_version = String::from_utf8_lossy(&node_version_output.expect("failed to get node output").stdout).replace("v", "");
-          let server_package_json: PackageJson = reqwest::blocking::get("https://registry.npmjs.org/guijs-version-marker/latest")
-            .expect("failed to read server package.json")
-            .json::<PackageJson>()
-            .expect("failed to parse server package.json");
-          let node_version_compare = tauri::api::version::compare(&node_version, &server_package_json.custom.min_node_version);
-          
-          if node_version_compare.is_ok() && node_version_compare.expect("failed to compare node versions") <= 0 {
-            let handle2 = webview.handle();
-            notify_state(&handle, String::from("splashscreen"));
-            std::thread::spawn(move || {
-              let update_deps = Arc::new(Mutex::new(Vec::new()));
-              let mut install_deps = Vec::new();
-              for (dependency, latest_version) in server_package_json.dev_dependencies.iter() {
-                let current_version = get_current_version(dependency.to_string());
-                if current_version.is_some() {
-                  let current_version_value = current_version.unwrap().replace(">", "").replace("=", "");
-                  let version_compare = tauri::api::version::compare(&current_version_value, &latest_version);
-                  if version_compare.is_ok() && version_compare.expect("failed to compare versions") == 1 {
-                    let mut deps = update_deps
-                      .lock()
-                      .expect("Failed to lock update_deps");
-                    deps.push(dependency.clone());
+        let node_path = which::which("node");
+        if node_path.is_ok() {
+          // check if node exists & matches the minimum version
+          let node_version_output = Command::new(node_path.unwrap())
+            .args(vec!("--version"))
+            .stdout(Stdio::piped())
+            .output();
+          if node_version_output.is_ok() {
+            let node_version = String::from_utf8_lossy(&node_version_output.expect("failed to get node output").stdout).replace("v", "");
+            let server_package_json: PackageJson = reqwest::blocking::get("https://registry.npmjs.org/guijs-version-marker/latest")
+              .expect("failed to read server package.json")
+              .json::<PackageJson>()
+              .expect("failed to parse server package.json");
+            let node_version_compare = tauri::api::version::compare(&node_version, &server_package_json.custom.min_node_version);
+            
+            if node_version_compare.is_ok() && node_version_compare.expect("failed to compare node versions") <= 0 {
+              let handle2 = webview.handle();
+              notify_state(&handle, String::from("splashscreen"));
+              std::thread::spawn(move || {
+                let update_deps = Arc::new(Mutex::new(Vec::new()));
+                let mut install_deps = Vec::new();
+                for (dependency, latest_version) in server_package_json.dev_dependencies.iter() {
+                  let current_version = get_current_version(dependency.to_string());
+                  if current_version.is_some() {
+                    let current_version_value = current_version.unwrap().replace(">", "").replace("=", "");
+                    let version_compare = tauri::api::version::compare(&current_version_value, &latest_version);
+                    if version_compare.is_ok() && version_compare.expect("failed to compare versions") == 1 {
+                      println!("found update from {} to {}", current_version_value, latest_version);
+                      let mut deps = update_deps
+                        .lock()
+                        .expect("Failed to lock update_deps");
+                      deps.push(dependency.clone());
+                    }
+                  } else {
+                    install_deps.push(dependency.clone());
                   }
-                } else {
-                  install_deps.push(dependency.clone());
                 }
-              }
 
-              if install_deps.len() > 0 {
-                notify_state(&handle, String::from("first-download"));
-                for dep in install_deps {
-                  install_dependency(dep.to_string());
-                }
-              }
-              if update_deps.lock().expect("Failed to lock update_deps").len() > 0 {
-                notify_state(&handle, String::from("update-available"));
-                tauri::event::listen(String::from("skip-update"), move |_| {
-                  spawn_guijs_server(&handle);
-                });
-                tauri::event::listen(String::from("update"), move |_| {
-                  notify_state(&handle2, String::from("downloading-update"));
-                  for dep in update_deps.lock().expect("Failed to lock update_deps").iter() {
-                    update_dependency(dep.to_string());
+                if install_deps.len() > 0 {
+                  notify_state(&handle, String::from("first-download"));
+                  for dep in install_deps {
+                    install_dependency(dep.to_string());
                   }
-                  
+                }
+                if update_deps.lock().expect("Failed to lock update_deps").len() > 0 {
+                  notify_state(&handle, String::from("update-available"));
+                  tauri::event::listen(String::from("skip-update"), move |_| {
+                    spawn_guijs_server(&handle);
+                  });
+                  tauri::event::listen(String::from("update"), move |_| {
+                    notify_state(&handle2, String::from("downloading-update"));
+                    for dep in update_deps.lock().expect("Failed to lock update_deps").iter() {
+                      update_dependency(dep.to_string());
+                    }
+                    
+                    spawn_guijs_server(&handle2);
+                  });
+                } else {
                   spawn_guijs_server(&handle2);
-                });
-              } else {
-                spawn_guijs_server(&handle2);
-              }
-            });
-          } else {
-            notify_state_with_payload(&handle, String::from("node-wrong-version"), format!("{}|{}", node_version, server_package_json.custom.min_node_version));
+                }
+              });
+            } else {
+              notify_state_with_payload(&handle, String::from("node-wrong-version"), format!("{}|{}", node_version, server_package_json.custom.min_node_version));
+            }
           }
         } else {
           notify_state(&handle, String::from("node-not-found"));
@@ -123,27 +125,26 @@ fn orchestrator_command() -> String {
 }
 
 fn get_current_version(dependency: String) -> Option<String> {
-  println!("getting {} version", dependency);
-  let output = Command::new(dependency.clone())
-    .args(vec!("--version"))
-    .stdout(Stdio::piped())
-    .output();
-  if output.is_ok() {
-    let stdout = &output.unwrap().stdout;
-    let output_str = String::from_utf8_lossy(stdout);
-    lazy_static! {
-      static ref RE: Regex = Regex::new(r"@(\d+).(\d+).(\d+)").unwrap();
+  let binary = dependency.replace("@guijs/", "guijs-").replace("-core", "");
+  println!("getting {} version, binary {}", dependency, binary);
+  let dependency_binary_path = which::which(binary.clone());
+  if dependency_binary_path.is_ok() {
+    println!("{:?}", which::which(binary.clone()).unwrap());
+    let output = Command::new(dependency_binary_path.unwrap())
+      .args(vec!("--version"))
+      .stdout(Stdio::piped())
+      .output();
+    if output.is_ok() {
+      let stdout = &output.unwrap().stdout;
+      let version = String::from_utf8_lossy(stdout);
+      println!("{} v{}", dependency, version);
+      Some(version.to_string())
+    } else {
+      println!("{} not installed", dependency);
+      None
     }
-    let caps = RE.captures(&output_str).unwrap();
-    let version = format!("{}.{}.{}",
-      caps.get(1).unwrap().as_str(),
-      caps.get(2).unwrap().as_str(),
-      caps.get(3).unwrap().as_str()
-    );
-    println!("{} v{}", dependency, version);
-    Some(version.to_string())
   } else {
-    println!("{} not installed", dependency);
+    println!("not found");
     None
   }
 }
@@ -151,8 +152,9 @@ fn get_current_version(dependency: String) -> Option<String> {
 fn run_npm_install(dependency: String, exists: bool) {
   let command = if exists { "update" } else { "install" };
   println!("{} {}", command, dependency);
-  let guijs_stdout = Command::new(orchestrator_command())
-    .args(vec!("install", dependency.as_str()))
+  let npm_path = which::which("npm").expect("failed to get npm path");
+  let guijs_stdout = Command::new(npm_path)
+    .args(vec!("install", "-g", dependency.as_str()))
     .stdout(Stdio::piped())
     .spawn().expect(&format!("failed to {} guijs server package", command))
     .stdout.expect(&format!("failed to get guijs {} stdout", command));
@@ -191,8 +193,9 @@ fn notify_state_with_payload<T: 'static>(handle:  &Handle<T>, name: String, payl
 }
 
 fn spawn_guijs_server<T: 'static>(handle: &Handle<T>) {
+  let guijs_server_path = which::which("guijs-server").unwrap();
   let stdout = Command::new(orchestrator_command())
-    .args(vec!("run", "guijs-server"))
+    .args(vec!("run", guijs_server_path.to_str().expect("guijs server path is not utf-8")))
     .stdout(Stdio::piped())
     .spawn()
     .expect("Failed to start guijs server")
