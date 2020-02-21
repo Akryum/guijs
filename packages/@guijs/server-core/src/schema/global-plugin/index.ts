ebbd08b4de44d61244105332b1ee120478cabd2c
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-02-21 14:51:11 +0100

    fix(plugins): clearModule

--- packages/@guijs/server-core/src/schema/global-plugin/index.ts
+++ packages/@guijs/server-core/src/schema/global-plugin/index.ts
@@ -3,14 +3,17 @@ import fs from 'fs-extra'
 import { rcFolder } from '@/util/rc-folder'
 import { resolveModule } from '@nodepack/module'
 import { installPackage } from '@nodepack/utils'
+import clearModule from 'clear-module'
 
 export const pluginFolder = path.join(rcFolder, 'global-plugins')
 fs.ensureDirSync(pluginFolder)
 
 export function isPluginInstalled (id: string) {
+  clearModule(id)
   return !!resolveModule(path.join(id, 'package.json'), pluginFolder)
 }
 
 export async function installPlugin (id: string) {
+  clearModule(id)
   await installPackage(pluginFolder, 'npm', null, id, false)
 }
