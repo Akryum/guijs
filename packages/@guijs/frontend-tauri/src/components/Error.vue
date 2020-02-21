1377204580aea51ac189cbbe070316ed1f0d4498
Lucas Fernandes Nogueira <lucas@tauri.studio>
2020-02-21 10:09:54 -0300

    feat(svg): use base64 (#17)
    Co-authored-by: nothingismagick <drthompsonsmagickindustries@gmail.com>

--- packages/@guijs/frontend-tauri/src/components/Error.vue
+++ packages/@guijs/frontend-tauri/src/components/Error.vue
@@ -12,7 +12,7 @@ export default {
 <template>
   <div class="flex flex-col items-center">
     <img
-      src="~@/assets/error.svg"
+      src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iOTIiIGhlaWdodD0iOTIiIHZpZXdCb3g9IjAgMCA5MiA5MiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTQ2IDBDMjAuNjA4IDAgMCAyMC42MDggMCA0NkMwIDcxLjM5MiAyMC42MDggOTIgNDYgOTJDNzEuMzkyIDkyIDkyIDcxLjM5MiA5MiA0NkM5MiAyMC42MDggNzEuMzkyIDAgNDYgMFpNNTAuNiA2OUg0MS40VjU5LjhINTAuNlY2OVpNNTAuNiA1MC42SDQxLjRWMjNINTAuNlY1MC42WiIgZmlsbD0iI0ZGRTFDNiIvPgo8L3N2Zz4K"
       alt="Downloading"
     >
 
