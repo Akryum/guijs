4839fb8be155a2850caf243692f4aea8f1e41f05
tensor-programming <tensordeveloper@gmail.com>
2020-02-23 18:30:47 -0500

    add webview and documentation to the powershellscript

--- packages/@guijs/tauri-app/src-tauri/bin/guijs-orchestrator-x86_64-pc-windows-msvc.ps1
+++ packages/@guijs/tauri-app/src-tauri/bin/guijs-orchestrator-x86_64-pc-windows-msvc.ps1
@@ -1,13 +1,28 @@
 #!/usr/bin/env pwsh
 
+# capture arguments
 $mode = $args[0]
 $path = $args[1]
-$appName = "guijs"
 
+# define process names
+$appName = "guijs"
+$webviewProcessName = "Win32WebViewHost"
+
+# if argument mode equals run
 if ($mode -eq "run") {
+    # start the server process without opening a new window
     $path_id = Start-Process -FilePath $path -PassThru -NoNewWindow 
     
+    # get the guijs process and wait until it stops before executing the rest of the code
     Get-Process -Name $appName, msiexec -ErrorAction SilentlyContinue | Wait-Process  
+    
+    # stop the server process
     Stop-Process -Id $path_id
-    stop-process -Id $PID
+
+    # stop the webview process
+    Stop-Process -Id $webviewProcessName
+
+    # stop the powershell process
+    Stop-process -Id $PID
+    
 }
